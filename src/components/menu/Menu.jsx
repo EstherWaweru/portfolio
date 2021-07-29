84dab9908d5e0e6fcf866fa9a3defd37b217db88
EstherWaweru <estherwaweru86@gmail.com>
2021-07-29 19:16:05 +0300

    Customizing sidebar menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,22 +4,22 @@ function Menu({open,setOpen}){
     return(
         <div className = {"menu " + (open && "active" )}>
             <ul>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#about">Home</a>
                 </li>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#projects">Projects</a>
                 </li>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#skills">Skills</a>
                 </li>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li onClick = {() => setOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
