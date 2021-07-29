1aeeee527e20b813302e234551b75c6a067d7fea
EstherWaweru <estherwaweru86@gmail.com>
2021-07-29 17:48:18 +0300

    Adding the hamburger menu in the nav bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,16 +8,22 @@ function Topbar(){
                 <div className = "left">
                     <a href="#skills" className = "logo">KarehTech</a>
                     <div className = "itemContainer">
-                    <Person className = "icon"/>
-                    <span> +254 711 111 111</span>
+                        <Person className = "icon"/>
+                        <span> +254 711 111 111</span>
                     </div>
                     <div className = "itemContainer">
                         <Mail className = "icon"/>
                         <span> karehtech@gmail.com</span>
                     </div>
                 </div>
-                Hello
-                <div className = "right"></div>
+                
+                <div className = "right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
 
         </div>
